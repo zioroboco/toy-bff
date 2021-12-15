c440b3a7bcb4627f9ca52fdeccdea470e85135b5
Louis Quinnell <_@ziorobo.co>
2021-12-15 22:32:17 +1100

    Call fetch from the mocked server

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,9 +1,14 @@
 import { expect, test } from "@playwright/test"
+import fetch from "node-fetch"
 
 test.describe(`demo`, () => {
   test.beforeEach(async ({ page, baseURL }) => {
-    await page.route("/thing", route => {
-      route.fulfill({ status: 200 })
+    await page.route("/thing", async route => {
+      const body = await fetch("https://httpbin.org/get")
+        .then(res => res.json())
+        .then(res => res.url)
+
+      route.fulfill({ status: 200, body })
     })
     await page.goto(baseURL)
     await page.waitForLoadState("networkidle")
@@ -17,6 +22,6 @@ test.describe(`demo`, () => {
     await page.waitForResponse("/thing")
 
     const result = await page.$("#result")
-    expect(await result.textContent()).toMatch("200")
+    expect(await result.textContent()).toMatch("https://httpbin.org/get")
   })
 })
