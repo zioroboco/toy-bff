6effb29e0907053f0212ff66f5b85d10b15b054b
Louis Quinnell <_@ziorobo.co>
2021-12-15 22:00:34 +1100

    Add a button that hits httpbin

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,13 +1,19 @@
 import { expect, test } from "@playwright/test"
 
-test.describe(`demo cases`, () => {
+test.describe(`demo`, () => {
   test.beforeEach(async ({ page, baseURL }) => {
     await page.goto(baseURL)
     await page.waitForLoadState("networkidle")
   })
 
   test(`simple`, async ({ page }) => {
-    const content = await page.content()
-    expect(content).toMatch("click me!")
+    const button = await page.$("#button")
+    expect(await button.textContent()).toMatch("click me!")
+
+    await button.click()
+    await page.waitForResponse("https://httpbin.org/get")
+
+    const result = await page.$("#result")
+    expect(await result.textContent()).toMatch("httpbin")
   })
 })
